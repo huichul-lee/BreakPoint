7c265d9c1c6973afa0c0637613dfeb3d217034f4
마론 lhc2272@hanmail.net <lhc2272@korea.com>
2018-01-30 20:53:03 +0900

    Initial Commit

---
+++ BreakPoint/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  BreakPoint
+//
+//  Created by huichulLee on 2018. 1. 30..
+//  Copyright © 2018년 lhc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var sum = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var count = 0
+        for row in 5...10 {
+            count += 1
+            self.sum += row
+        }
+        print("총 합은 \(self.sum), \(count)회 실행되었습니다.")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
